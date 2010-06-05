d72e9bc64b01b5694e708f850b724e0d5d3063eb
Ariel H. Pillet <apillet@gmail.com>
2010-06-05 04:37:16 -0300

    minor refactoring

--- rubymorph.rb
+++ rubymorph.rb
@@ -28,9 +28,8 @@ module RubyMorph
   # and handles events and drawing.
   #
   class RubyMorph < Gosu::Window
-    
-    def initialize
-      
+   
+    def initialize      
       super(ScreenWidth, ScreenHeight, false, 20)
       self.caption = "RubyMorph | Biomorphs in the land of Ruby / Gosu"
        
@@ -41,18 +40,15 @@ module RubyMorph
       @save_suffix = 1
       
       # initial selection from default parent gene
-      self.select(Gene.new)
-          
+      self.select(Gene.new)          
     end
     
     # from a given selected parent, reproduce and redisplay
-    def select(parent)
-      
+    def select(parent)     
       x = 0
       y = 0
       width = ScreenWidth / 4
-      height = ScreenHeight / 3
-      
+      height = ScreenHeight / 3      
       genes = parent.reproduce(11)
       
       @panels = Array.new
@@ -64,26 +60,22 @@ module RubyMorph
         end
         
        @panels << Panel.new(self, x, y, width, height, genes[i])
-      end
-      
+      end      
     end
     
     # gosu draw
     def draw
-      
       # draw the cursor
       @cursor.draw(self.mouse_x, self.mouse_y, 5.0, 0.75,0.75)
        
       # draw each canvas
       @panels.each do |panel|
         panel.draw
-      end
-       
+      end       
     end
     
     # recursive tree drawing method
     def draw_tree(x, y, length, dir, dx, dy)
-    
       if dir < 0
         dir = dir + 8
       elsif dir >= 8
@@ -100,109 +92,73 @@ module RubyMorph
         self.draw_tree(xnew, ynew, length - 1, dir - 1, dx, dy)
         self.draw_tree(xnew, ynew, length - 1, dir + 1, dx, dy)
       end
-      
     end
     
     def button_down(id)
-      if id == Gosu::MsLeft || id == Gosu::MsRight
-        @panels.each do |panel|
-          if panel.clicked?(mouse_x, mouse_y)
-            self.select(panel.gene)
-          end
-        end
-      elsif id == Gosu::KbS
-        @panels.each do |panel|
-          if panel.clicked?(mouse_x, mouse_y)
-            File.open("rubymorph-#{@save_suffix}.yaml", "w") do |file| 
-              YAML.dump(panel.gene.to_yaml, file)
-            end
-            @save_suffix += 1
-          end
-        end
-      elsif id == Gosu::KbEscape
-        self.close
-      end
-      
+      select(panel.gene) if id == Gosu::MsLeft || id == Gosu::MsRight
+      save(panel.gene)   if id == Gosu::KbS
+      close              if id == Gosu::KbEscape
     end
-    
+
+    def panel
+      @panels.detect {|panel| panel.clicked?(mouse_x, mouse_y) }
+    end
+
+    def save gene
+      File.open("rubymorph-#{@save_suffix}.yaml", "w") do |file|
+        YAML.dump(gene.to_yaml, file)
+      end
+      @save_suffix += 1
+    end
+
     # draws a box on screen
     def draw_box(x, y, width, height, colour, z = 0.0)
-       self.draw_line(x, y, colour, x + width, y, colour, z)
-       self.draw_line(x + width, y, colour, x + width, y + height, colour, z)
-       self.draw_line(x, y, colour, x, y + height, colour, z)
-       self.draw_line(x, y + height, colour, x + width, y + height, colour, z)
+      self.draw_line(x, y, colour, x + width, y, colour, z)
+      self.draw_line(x + width, y, colour, x + width, y + height, colour, z)
+      self.draw_line(x, y, colour, x, y + height, colour, z)
+      self.draw_line(x, y + height, colour, x + width, y + height, colour, z)
     end
-    
+
     # returns a Gosu::Color for a given length
     # @todo encode colour as genes
     def branch_colour(length)
-      if length == 0
-        colour(:white)
-      elsif length == 1
-        colour(:yellow)
-      elsif length == 2
-        colour(:blue)
-      elsif length == 3
-        colour(:green)
-      elsif length == 4
-        colour(:white)
+      case length
+      when 0 then Gosu::Color::WHITE
+      when 1 then Gosu::Color::YELLOW
+      when 2 then Gosu::Color::BLUE
+      when 3 then Gosu::Color::GREEN
+      when 4 then Gosu::Color::WHITE
       else
-      colour(:red)  
+        Gosu::Color::RED
       end
     end
-      
-    # returns a Gosu::Color for a given colour symbol
-    def colour(symbol)
-      if symbol == :black
-        Gosu::Color.new(0xff000000)
-      elsif symbol == :white
-        Gosu::Color.new(0xffffffff)
-      elsif symbol == :red
-        Gosu::Color.new(0xffff0000)
-      elsif symbol == :blue
-        Gosu::Color.new(0xff0000ff)
-      elsif symbol == :green
-        Gosu::Color.new(0xff00ff00)
-      elsif symbol == :yellow
-        Gosu::Color.new(0xffffff00)
-      elsif symbol == :gray
-        Gosu::Color.new(0xff808080)
-      else
-        Gosu::Color.new(0xff000000)  # unknown colour defaults to black
-      end
-      
-    end
-    
   end
   
   # A panel which displays a given gene.
   #
-  class Panel
-    
+  class Panel    
     attr_accessor :gene
     
     def initialize(window, x, y, width, height, gene)
-      @window, @x, @y, @width, @height, @gene = window, x, y, width, height, gene
+      @window = window
+      @x      = x
+      @y      = y
+      @width  = width
+      @height = height
+      @gene   = gene
     end
-    
+
     def clicked?(x, y)
-      if x >= @x && x <= @x + @width
-        if y >= @y && y <= @y + @height
-          return true
-        end
-      end
-      false
+      x >= @x && x <= @x + @width and y >= @y && y <= @y + @height
     end
     
     def draw
-      @window.draw_box(@x, @y, @width, @height, @window.colour(:gray), 1.0)
+      @window.draw_box(@x, @y, @width, @height, Gosu::Color::GRAY, 1.0)
       @window.draw_tree(@x + (@width / 2), @y + (@height / 2), @gene.length, @gene.direction, @gene.dx, @gene.dy)
     end
-    
   end  
 
-  class Gene < Array
-  
+  class Gene < Array  
     def initialize
       # default parent initial genes
       9.times do |i|
@@ -274,13 +230,12 @@ module RubyMorph
         children << child
       end
       children
-    end
-    
+    end    
   end
-
-  rubymorph = RubyMorph.new
-  rubymorph.show
-  puts("I for one welcome our new biomorphic overlords...thanks and goodbye!")
-  
 end
 
+if $0 == __FILE__
+  rubymorph = RubyMorph::RubyMorph.new
+  rubymorph.show
+  puts("I for one welcome our new biomorphic overlords...thanks and goodbye!")
+end
